621f77bad714e617fce65dadd504758505cb2049
BogdanZ19 <bogdanzainia1@gmail.com>
2026-08-27 10:56:48 +0300

    implement command handler logic and login handler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
+import { CommandsRegistry, registerCommand, runCommand } from "./commands/commands.js";
+import { handlerLogin } from "./commands/users.js";
 import { Config, readConfig, setUser } from "./config.js";
 
 function main() {
-    setUser("test");
-    const cfg: Config = readConfig();
-    console.log(cfg)
-}
+    // setUser("test");
+    // const cfg: Config = readConfig();
+    // console.log(cfg)
+
+    let args = process.argv.slice(2);
+    
+    if (args.length < 1) {
+        console.log("usage: cli <command> [args...]");
+        process.exit(1);
+    }
+    
+    const commandName = args[0];
+    const cmdArgs = args.slice(1);
+    const cmdRegistry: CommandsRegistry = {};
+    
+    registerCommand(cmdRegistry, "login", handlerLogin);
+    runCommand(cmdRegistry, commandName, ...cmdArgs);
+}       
 
 main();
